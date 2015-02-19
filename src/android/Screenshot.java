6b6bd92439d2b5cdf9eed7d282b87cebfe07baaa
Adrien Grandemange <adrien.grandemange@oyez.fr>
2015-02-19 16:24:14 +0100

    Simplify plugin (support only android and URI screenshots), make it asynchronous

--- src/android/Screenshot.java
+++ src/android/Screenshot.java
@@ -1,17 +1,9 @@
-/**
- * Copyright (C) 2012 30ideas (http://30ide.as)
- * MIT licensed
- * 
- * @author Josemando Sobral
- * @created Jul 2nd, 2012.
- * improved by Hongbo LU
- */
-package com.darktalker.cordova.screenshot;
+package fr.oyez.cordova.screenshot;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.lang.StringBuilder;
+import java.util.concurrent.FutureTask;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -20,125 +12,90 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import android.util.Log;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Matrix;
 import android.os.Environment;
 import android.util.Base64;
+import android.util.Base64OutputStream;
 import android.view.View;
 import android.opengl.GLES20;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class Screenshot extends CordovaPlugin {
+    private int width;
+    private int height;
+    private ByteBuffer buffer;
+    private Bitmap transferBmp;
+    private String base64Header = "data:image/jpeg;base64,";
 
-	@Override
-	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-	 	// starting on ICS, some WebView methods
-		// can only be called on UI threads
+    @Override
+    protected void pluginInitialize() {
+        width = webView.getWidth();
+        height = webView.getHeight();
+        buffer = ByteBuffer.allocateDirect(width * height * 4);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        transferBmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    }
 
-		if (action.equals("saveScreenshot")) {
-			final String format = (String) args.get(0);
-			final Integer quality = (Integer) args.get(1);
-			final String fileName = (String)args.get(2);
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("takeScreenshot")){
+            final Integer quality = (Integer) args.get(0);
+            final Integer scaleDown = (Integer) args.get(1);
 
-			super.cordova.getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						if(format.equals("png") || format.equals("jpg")){
-                            int width = webView.getWidth();
-                            int height = webView.getHeight();
-                         
-                            ByteBuffer buf = ByteBuffer.allocateDirect(width * height * 4);
-                            buf.order(ByteOrder.LITTLE_ENDIAN);
-                            GLES20.glReadPixels(0, 0, width, height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buf);
-                            
-                            Bitmap src = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-                            src.copyPixelsFromBuffer(buf);
-                            
-                            Matrix matrix = new Matrix();
-                            matrix.preScale(1.0f, -1.0f);
-                            
-                            Bitmap bitmap = Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
-							
-                            File folder = new File(Environment.getExternalStorageDirectory(), "Pictures");
-							if (!folder.exists()) {
-								folder.mkdirs();
-							}
+            FutureTask future = new FutureTask(new Runnable() {
+                @Override
+                public void run() {
+                    buffer.clear();
+                    GLES20.glReadPixels(0, 0, width, height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+                }
+            }, null);
 
-							File f = new File(folder, fileName + "."+format);
+            cordova.getActivity().runOnUiThread(future);
+            try {
+                future.get();
+            }
+            catch (Exception e) {
+                callbackContext.error(e.getMessage());
+                return false;
+            }
 
-							FileOutputStream fos = new FileOutputStream(f);
-							if(format.equals("png")){
-								bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-							}
-							if(format.equals("jpg")){
-								bitmap.compress(Bitmap.CompressFormat.JPEG, quality == null?100:quality, fos);
-							}
-							JSONObject jsonRes = new JSONObject();
-							jsonRes.put("filePath",f.getAbsolutePath());
-				                        PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
-				                        callbackContext.sendPluginResult(result);
-						}else{
-							callbackContext.error("format "+format+" not found");
-							
-						}
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        transferBmp.copyPixelsFromBuffer(buffer);
 
-					} catch (JSONException e) {
-						callbackContext.error(e.getMessage());
-						
-					} catch (IOException e) {
-						callbackContext.error(e.getMessage());
-						
-					}
-				}
-			});
-			return true;
-		}else if(action.equals("getScreenshotAsURI")){
-			final Integer quality = (Integer) args.get(0);
-			
-			super.cordova.getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					View view = webView.getRootView();
-					try {
-						view.setDrawingCacheEnabled(true);
-						Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
-						view.setDrawingCacheEnabled(false);
+                        int tgtWidth = width / scaleDown;
+                        int tgtHeight = height / scaleDown;
 
-						ByteArrayOutputStream jpeg_data = new ByteArrayOutputStream();
-						
-						if (bitmap.compress(CompressFormat.JPEG, quality, jpeg_data)) {
-						   byte[] code = jpeg_data.toByteArray();
-						   byte[] output = Base64.encode(code, Base64.NO_WRAP);
-						   String js_out = new String(output);
-						   js_out = "data:image/jpeg;base64," + js_out;
-						   JSONObject jsonRes = new JSONObject();
-						   jsonRes.put("URI", js_out);
-				                   PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
-				                   callbackContext.sendPluginResult(result);
-							
-						   js_out = null;
-						   output = null;
-						   code = null;
-						}
-						
-						jpeg_data = null;
+                        Bitmap scaled = Bitmap.createScaledBitmap(transferBmp, tgtWidth, tgtHeight, true);
 
-					} catch (JSONException e) {
-						callbackContext.error(e.getMessage());
-						
-					} catch (Exception e) {
-						callbackContext.error(e.getMessage());
-						
-					}
-				}
-			});
+                        Matrix matrix = new Matrix();
+                        matrix.preScale(1.0f, -1.0f);
 
-			return true;		
-		}
-		callbackContext.error("action not found");
-		return false;
-	}
+                        Bitmap bitmap = Bitmap.createBitmap(scaled, 0, 0, tgtWidth, tgtHeight, matrix, false);
+                        ByteArrayOutputStream jpeg_data = new ByteArrayOutputStream();
+                        Base64OutputStream raw_jpeg = new Base64OutputStream(jpeg_data, Base64.NO_WRAP);
+                        if (bitmap.compress(CompressFormat.JPEG, quality, raw_jpeg)) {
+                            StringBuilder sb = new StringBuilder(base64Header.length() + jpeg_data.size());
+                            sb.append(base64Header);
+                            sb.append(jpeg_data.toString());
+                            PluginResult result = new PluginResult(PluginResult.Status.OK, sb.toString());
+                            callbackContext.sendPluginResult(result);
+                        }
+                    } catch (Exception e) {
+                        callbackContext.error(e.getMessage());
+                    }
+                }
+            });
+
+            return true;
+        }
+        callbackContext.error("Action not found");
+        return false;
+    }
 }
